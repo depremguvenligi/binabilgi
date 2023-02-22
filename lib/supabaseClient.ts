353f62402fb440d5eb30e9c3999e8e06e5559bc0
Yamac <yamac@ceng.metu.edu.tr>
2023-02-22 21:46:34 +0300

    style: lose the ternary operator

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.SUPABASE_API_URL!
-    ? process.env.SUPABASE_API_URL!
-    : "http://localhost:54321",
+  process.env.SUPABASE_API_URL || "http://localhost:54321",
   process.env.SUPABASE_KEY!
 );
