11d59740f261fbf286fd3aefbf97d95cf78ac755
Ant Somers <antsomers@gmail.com>
2023-02-22 03:13:57 +0300

    feat: add app page

---
+++ pages/app.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+
+import OlLayers from "@/components/OlLayers";
+import OlMap from "@/components/OlMap";
+import OlTileLayer from "@/components/OlTileLayer";
+import OSMTile from "@/lib/OSMTile";
+import WMSTile from "@/lib/WMSTile";
+
+const BinaBilgi = () => {
+  return (
+    <div>
+      <Head>
+        <title>Bina Bilgi</title>
+      </Head>
+      <OlMap center={[0, 0]} zoom={12}>
+        <OlLayers>
+          <OlTileLayer source={OSMTile()} />
+          <OlTileLayer
+            source={WMSTile({
+              url: "https://ahocevar.com/geoserver/wms",
+              params: { LAYERS: "topp:states", TILED: true },
+              serverType: "geoserver",
+              // Countries have transparency, so do not fade tiles:
+              transition: 0,
+            })}
+          />
+        </OlLayers>
+      </OlMap>
+    </div>
+  );
+};
+
+export default BinaBilgi;
