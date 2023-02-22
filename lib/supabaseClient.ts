a4a225fb619db70e3efbce5b8127fef8606e7eb3
Yamac <yamac@ceng.metu.edu.tr>
2023-02-22 21:09:14 +0300

    chore: move supabase API URL to env

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:54321"
-    : `https://${process.env.SUPABASE_PROJECT_ID!}.supabase.co`,
+  process.env.SUPABASE_API_URL!
+    ? process.env.SUPABASE_API_URL!
+    : "http://localhost:54321",
   process.env.SUPABASE_KEY!
 );
