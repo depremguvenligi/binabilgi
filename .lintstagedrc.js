461ff760cf5a247bf5216507be75c4ceb955552c
Ant Somers <antsomers@gmail.com>
2023-02-28 17:19:45 +0300

    chore: add css & sql extensions to lintstaged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,5 +7,5 @@ const buildEslintCommand = (filenames) =>
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
-  "*.{md,json,graphql}": "prettier --write",
+  "*.{md,json,graphql,css,sql}": "prettier --write",
 };
