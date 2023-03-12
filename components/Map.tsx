165de7f502bb12456c8b1eb3287309d07168a7d7
Ant Somers <antsomers@gmail.com>
2023-03-13 01:43:50 +0300

    feat: add IBB geology tileset

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,7 +2,7 @@ import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
 
 import LocationMarker from "./LocationMarker";
 import MinimapControl from "./MiniMap";
@@ -14,10 +14,20 @@ const Map = () => {
       zoom={13}
       className="w-full h-screen"
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <LayersControl position="bottomright">
+        <LayersControl.Overlay name="OSM" checked>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="IBB Geology">
+          <TileLayer
+            attribution='&copy; <a href="https://www.ibb.gov.tr">İstanbul Büyükşehir Belediyesi</a> contributors'
+            url="/tiles/ibb-geology/{z}/{x}/{y}.png"
+          />
+        </LayersControl.Overlay>
+      </LayersControl>
       <LocationMarker />
       <MinimapControl position="topright" />
     </MapContainer>
