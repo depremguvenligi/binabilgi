42d08d37b489c7e16003ba9ccc5f5a89c9d3c452
Ant Somers <antsomers@gmail.com>
2023-03-14 01:47:56 +0300

    feat: add soilprime PGA data

--- components/LocationMarker.tsx
+++ components/LocationMarker.tsx
@@ -2,26 +2,46 @@ import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet/dist/leaflet.css";
 
+import axios from "axios";
 import { LatLng } from "leaflet";
 import { useEffect, useState } from "react";
 import { Marker, Popup, useMapEvents } from "react-leaflet";
 
+type SoilPrimeAPIResponse = {
+  PGA: number;
+  PGV: number;
+  SS: number;
+  S1: number;
+};
+
 const LocationMarker = () => {
   const [position, setPosition] = useState<LatLng>();
+  const [pga, setPga] = useState<number>();
   const map = useMapEvents({
     locationfound(e) {
       setPosition(e.latlng);
       map.flyTo(e.latlng, map.getZoom());
+      axios
+        .get(
+          `http://api.soilprime.com/seismic_hazard?dyhd_no=2&latitude=${e.latlng.lat}&longitude=${e.latlng.lng}`
+        )
+        .then((res) => {
+          const data = res.data as SoilPrimeAPIResponse;
+          setPga(data.PGA);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   });
-  
+
   useEffect(() => {
     map.locate();
   }, [map]);
 
   return position === null ? null : (
     <Marker position={position || { lat: 51.505, lng: -0.09 }}>
-      <Popup>You are here</Popup>
+      <Popup>PGA: {pga}</Popup>
     </Marker>
   );
 };
