ebbf25d2204877558915ecd89031cec5759e69f8
Ant Somers <thiras@users.noreply.github.com>
2023-03-19 11:22:40 +0300

    feat: add gem gaf (#43)
    * feat: add GEM GAF geojson
    
    * style: add weight to GEMLayer
    
    * style: raise GEMLayer weight to 2
    
    * feat: add layer control to app
    
    * feat: enable edge functions for vector-tile
    
    * chore: rename vector-tile to gem
    
    * Revert "feat: enable edge functions for vector-tile"
    
    This reverts commit f72806e4a9a069f319ba159d00c5724cef771af2.
    
    * feat: add attribution to GEMLayer
    
    * Revert "feat: add attribution to GEMLayer"
    
    This reverts commit 86baf4b857a25a50a2c1203da684688004d51c61.
    
    * refactor: use react-leaflet hooks at GEMLayer
    
    * feat: add fault type colors to GemLayer
    
    * feat: bindPopup to GemLayer
    
    * style: fix MapContainer CSS
    
    * style: raise GemLayer weight to 3
    
    * feat: add attribution to GemLayer

--- components/GemLayer.tsx
+++ components/GemLayer.tsx
@@ -1,26 +1,101 @@
-import { useLeafletContext } from "@react-leaflet/core";
+import {
+  createContainerComponent,
+  createElementHook,
+  createPathHook,
+  LeafletContextInterface,
+} from "@react-leaflet/core";
 import vectorTileLayer from "leaflet-vector-tile-layer";
-import { useEffect } from "react";
+import { defaultFeatureLayer } from "leaflet-vector-tile-layer";
 
-type Props = {
-  url: string;
+var faultColors: { [key: string]: string } = {
+  Anticline: "grey",
+  "Blind Thrust": "black",
+  Dextral: "blue",
+  "Dextral Transform": "blue",
+  "Dextral-Normal": "blue",
+  "Dextral-Oblique": "blue",
+  "Dextral-Reverse": "blue",
+  Normal: "red",
+  "Normal-Dextral": "red",
+  "Normal-Sinistral": "red",
+  "Normal-Strike-Slip": "red",
+  Reverse: "black",
+  "Reverse-Dextral": "black",
+  "Reverse-Sinistral": "black",
+  "Reverse-Strike-Slip": "black",
+  Sinistral: "#b936ff",
+  "Sinistral Transform": "#b936ff",
+  "Sinistral-Normal": "#b936ff",
+  "Sinistral-Reverse": "#b936ff",
+  "Spreading Ridge": "red",
+  "Strike-Slip": "yellow",
+  "Subduction Thrust": "black",
+  Syncline: "grey",
+  "": "green",
 };
 
-const GEMLayer = ({ url }: Props) => {
-  const context = useLeafletContext();
+const createVectorTileLayer = (
+  props: { [key: string]: any },
+  context: LeafletContextInterface
+) => {
+  const layer = vectorTileLayer(props.url, {
+    featureToLayer: (
+      feature: any,
+      layerName: string,
+      pxPerExtent: any,
+      options: { [key: string]: any }
+    ) => {
+      const layer = defaultFeatureLayer(
+        feature,
+        layerName,
+        pxPerExtent,
+        options
+      );
+      layer.bindPopup(
+        // List all properties of the feature
+        Object.keys(feature.properties)
+          .map((key) => {
+            return `<b>${key}</b>: ${feature.properties[key]}`;
+          })
+          .join("<br />")
+      );
 
-  useEffect(() => {
-    const layer = vectorTileLayer(url);
-    const container = context.layerContainer || context.map;
-
-    container.addLayer(layer);
-
-    return () => {
-      container.removeLayer(layer);
-    };
+      return layer;
+    },
+    style: (feature: any) => {
+      return {
+        color: faultColors[feature.properties.slip_type] || "green",
+        weight: 3,
+        opacity: 1,
+        interactive: true,
+      };
+    },
+    ...props.options,
   });
 
-  return null;
+  return {
+    instance: layer,
+    context,
+  };
 };
 
-export default GEMLayer;
+const updateVectorTileLayer = (
+  instance: typeof vectorTileLayer,
+  props: { [key: string]: any },
+  prevProps: { [key: string]: any }
+) => {
+  if (props.options !== prevProps.options) {
+    instance.setStyle(props.options);
+  }
+};
+
+const useVectorTileLayerElement = createElementHook(
+  createVectorTileLayer,
+  updateVectorTileLayer
+);
+
+const useVectorTileLayer = createPathHook(useVectorTileLayerElement);
+
+const GemLayer = createContainerComponent(useVectorTileLayer);
+
+export default GemLayer;
--- components/Map.tsx
+++ components/Map.tsx
@@ -2,26 +2,37 @@ import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import {
+  LayerGroup,
+  LayersControl,
+  MapContainer,
+  TileLayer,
+} from "react-leaflet";
 
-import GEMLayer from "./GemLayer";
+import GemLayer from "./GemLayer";
 import LocationMarker from "./LocationMarker";
 import MinimapControl from "./MiniMap";
 
 const Map = () => {
   return (
     <MapContainer
-      center={{ lat: 41.00, lng: 28.98 }}
+      center={{ lat: 41.0, lng: 28.98 }}
       zoom={13}
-      className="w-full h-screen"
+      className="w-full h-full"
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <GEMLayer url="/api/vector-tile/{z}/{x}/{y}.pbf" />
-      <LocationMarker />
       <MinimapControl position="topright" />
+      <LayersControl position="bottomright">
+        <LayersControl.Overlay name="GEM">
+          <LayerGroup attribution='&copy; <a href="https://github.com/GEMScienceTools/gem-global-active-faults">GEM</a>'>
+            <GemLayer url="/api/gem/{z}/{x}/{y}.pbf" />
+          </LayerGroup>
+        </LayersControl.Overlay>
+      </LayersControl>
+      <LocationMarker />
     </MapContainer>
   );
 };
