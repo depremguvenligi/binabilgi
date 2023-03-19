de052f3d657526f07b0e403c08eaec415036997f
Ant Somers <thiras@users.noreply.github.com>
2023-03-19 11:22:40 +0300

    feat: add gem gaf (#43)
    * feat: add GEM GAF geojson
    
    * style: add weight to GEMLayer
    
    * style: raise GEMLayer weight to 2
    
    * feat: add layer control to app
    
    * feat: enable edge functions for vector-tile
    
    * chore: rename vector-tile to gem
    
    * Revert "feat: enable edge functions for vector-tile"
    
    This reverts commit f72806e4a9a069f319ba159d00c5724cef771af2.
    
    * feat: add attribution to GEMLayer
    
    * Revert "feat: add attribution to GEMLayer"
    
    This reverts commit 86baf4b857a25a50a2c1203da684688004d51c61.
    
    * refactor: use react-leaflet hooks at GEMLayer
    
    * feat: add fault type colors to GemLayer
    
    * feat: bindPopup to GemLayer
    
    * style: fix MapContainer CSS
    
    * style: raise GemLayer weight to 3
    
    * feat: add attribution to GemLayer

---
+++ pages/api/gem/[...zxy].ts
@@ -0,0 +1,36 @@
+import { readFileSync } from "fs";
+import geojsonvt from "geojson-vt";
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import vtpbf from "vt-pbf";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { zxy } = req.query;
+
+  if (zxy === undefined || typeof zxy === "string")
+    return res.status(400).json({ error: "Bad Request" });
+
+  const file = path.join(
+    process.cwd(),
+    "public",
+    "gem_active_faults_harmonized.geojson"
+  );
+
+  const data = readFileSync(file, "utf8");
+
+  const tileIndex = geojsonvt(JSON.parse(data), {});
+  const tile = tileIndex.getTile(+zxy[0], +zxy[1], +zxy[2].replace(".pbf", ""));
+
+  if (tile === null) return res.status(404).json({ error: "Not Found" });
+
+  const buff = vtpbf.fromGeojsonVt({ geojsonLayer: tile });
+
+  res.setHeader("Content-Type", "application/x-protobuf");
+  res.setHeader("Content-Length", buff.length);
+  res.setHeader("Content-Disposition", `attachment; filename=${zxy[2]}`);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "s-maxage=86400");
+  res.end(buff);
+};
+
+export default handler;
