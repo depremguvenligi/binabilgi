5b73e75ce9447843afaad130cea39c2f6fe130b2
Ant Somers <antsomers@gmail.com>
2023-03-21 01:40:16 +0300

    refactor: downloadGemGaf

--- lib/downloadGemGaf.ts
+++ lib/downloadGemGaf.ts
@@ -1,11 +1,8 @@
 import fs from "fs";
 import https from "https";
-import path from "path";
 
 const downloadGemGaf = () => {
-  const file = fs.createWriteStream(
-    path.join(process.cwd(), "public", "gem_active_faults.geojson")
-  );
+  const file = fs.createWriteStream("gem_active_faults.geojson");
   https.get(
     "https://raw.githubusercontent.com/depremguvenligi/gem-global-active-faults/master/geojson/gem_active_faults.geojson",
     (response) => {
