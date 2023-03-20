1b794634e552159c6816d8c2026b9d76e0f5725e
Ant Somers <antsomers@gmail.com>
2023-03-21 00:07:23 +0300

    fix: downloadGemGaf call

--- lib/downloadGemGaf.ts
+++ lib/downloadGemGaf.ts
@@ -1,16 +1,19 @@
 import axios from "axios";
 import fs from "fs";
 
-axios
-  .get(
-    "https://raw.githubusercontent.com/depremguvenligi/gem-global-active-faults/master/geojson/gem_active_faults.geojson"
-  )
-  .then((response) => {
-    fs.writeFileSync(
-      "public/gem_active_faults.geojson",
-      JSON.stringify(response.data)
-    );
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const downloadGemGaf = () =>
+  axios
+    .get(
+      "https://raw.githubusercontent.com/depremguvenligi/gem-global-active-faults/master/geojson/gem_active_faults.geojson"
+    )
+    .then((response) => {
+      fs.writeFileSync(
+        "public/gem_active_faults.geojson",
+        JSON.stringify(response.data)
+      );
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+downloadGemGaf();
