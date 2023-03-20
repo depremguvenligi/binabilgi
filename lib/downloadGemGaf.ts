fa347b03cc22bbcad32b76219a7fa8b1457f31ca
Ant Somers <antsomers@gmail.com>
2023-03-21 00:04:15 +0300

    feat: add downloadGemGaf lib

---
+++ lib/downloadGemGaf.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import fs from "fs";
+
+axios
+  .get(
+    "https://raw.githubusercontent.com/depremguvenligi/gem-global-active-faults/master/geojson/gem_active_faults.geojson"
+  )
+  .then((response) => {
+    fs.writeFileSync(
+      "public/gem_active_faults.geojson",
+      JSON.stringify(response.data)
+    );
+  })
+  .catch((error) => {
+    console.log(error);
+  });
